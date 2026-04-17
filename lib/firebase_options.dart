eb7e304bbe6fa9be2527d58266acc7dc03bc07fe
QuackityDuck <84103520+Navaneeth-Git@users.noreply.github.com>
2026-04-17 15:48:34 +0530

    Update firebase_options.dart

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -5,6 +5,7 @@ import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart'
     show defaultTargetPlatform, kIsWeb, TargetPlatform;
 
+/// Default [FirebaseOptions] for use with your Firebase apps.
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     if (kIsWeb) {
@@ -18,12 +19,16 @@ class DefaultFirebaseOptions {
       case TargetPlatform.macOS:
         return macos;
       case TargetPlatform.windows:
-        return web;
+        throw UnsupportedError(
+          'DefaultFirebaseOptions not configured for windows.',
+        );
       case TargetPlatform.linux:
-        return web;
+        throw UnsupportedError(
+          'DefaultFirebaseOptions not configured for linux.',
+        );
       default:
         throw UnsupportedError(
-          'DefaultFirebaseOptions are not supported for this platform.',
+          'DefaultFirebaseOptions not supported for this platform.',
         );
     }
   }
